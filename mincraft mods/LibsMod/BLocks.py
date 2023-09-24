f8f24b6b639fde2fb75202a6004901e577bea7db
scratcheurs25 <126276357+scratcheurs25@users.noreply.github.com>
2023-09-24 19:04:03 +0200

    Update BLocks.py

--- mincraft mods/LibsMod/BLocks.py
+++ mincraft mods/LibsMod/BLocks.py
@@ -4,6 +4,11 @@ import game.main as Game
 
 def makeBLocksInWord(block:ModBlock.block):
     Game.blocks.append(block)
+class Block(ModBlock.block):
+    def __init__(self, id):
+        super().__init__(0,0,id)
+    def get(self,x,y):
+        return ModBlock.block(x,y,self.id)
 
 
 def dirt(x,y):
